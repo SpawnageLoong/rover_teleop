44d3fb747e8ad88b68574b36457da88103ca52f0
SpawnageLoong <richardloongcj@gmail.com>
2023-09-12 15:46:57 +0800

    Change to stepped values, change keybinds

--- rover_teleop/rover_teleop.py
+++ rover_teleop/rover_teleop.py
@@ -49,56 +49,32 @@ This node takes keypresses from the keyboard and publishes them
 as Twist/TwistStamped messages. It works best with a US keyboard layout.
 ---------------------------
 Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
-
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
-
-t : up (+z)
-b : down (-z)
+        w     
+   a    s    d
 
 anything else : stop
 
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
+r/f : increase/decrease all steps by 0.1
+t/g : increase/decrease only linear steps by 0.1
+y/h : increase/decrease only angular steps by 0.1
 
 CTRL-C to quit
 """
 
 moveBindings = {
-    'i': (1, 0, 0, 0),
-    'o': (1, 0, 0, -1),
-    'j': (0, 0, 0, 1),
-    'l': (0, 0, 0, -1),
-    'u': (1, 0, 0, 1),
-    ',': (-1, 0, 0, 0),
-    '.': (-1, 0, 0, 1),
-    'm': (-1, 0, 0, -1),
-    'O': (1, -1, 0, 0),
-    'I': (1, 0, 0, 0),
-    'J': (0, 1, 0, 0),
-    'L': (0, -1, 0, 0),
-    'U': (1, 1, 0, 0),
-    '<': (-1, 0, 0, 0),
-    '>': (-1, -1, 0, 0),
-    'M': (-1, 1, 0, 0),
-    't': (0, 0, 1, 0),
-    'b': (0, 0, -1, 0),
+    'w': (1, 0, 0, 0),
+    'a': (0, 0, 0, 1),
+    'd': (0, 0, 0, -1),
+    's': (-1, 0, 0, 0),
 }
 
 speedBindings = {
-    'q': (1.1, 1.1),
-    'z': (.9, .9),
-    'w': (1.1, 1),
-    'x': (.9, 1),
-    'e': (1, 1.1),
-    'c': (1, .9),
+    'r': (0.1, 0.1),
+    'f': (-0.1, -0.1),
+    't': (0.1, 0),
+    'g': (-0.1, 0),
+    'y': (0, 0.1),
+    'h': (0, -0.1),
 }
 
 
@@ -126,8 +102,8 @@ def restoreTerminalSettings(old_settings):
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
 
-def vels(speed, turn):
-    return 'currently:\tspeed %s\tturn %s ' % (speed, turn)
+def vels(speed_step, turn_step):
+    return 'current step sizes:\tspeed_step %s\tturn_step %s ' % (round(speed_step, 1), round(turn_step, 1))
 
 
 def main():
@@ -153,13 +129,14 @@ def main():
     spinner = threading.Thread(target=rclpy.spin, args=(node,))
     spinner.start()
 
-    speed = 0.5
-    turn = 1.0
+    speed_step = 0.1
+    turn_step = 0.1
     x = 0.0
     y = 0.0
     z = 0.0
     th = 0.0
     status = 0.0
+    stop = False
 
     twist_msg = TwistMsg()
 
@@ -172,7 +149,7 @@ def main():
 
     try:
         print(msg)
-        print(vels(speed, turn))
+        print(vels(speed_step, turn_step))
         while True:
             key = getKey(settings)
             if key in moveBindings.keys():
@@ -181,30 +158,35 @@ def main():
                 z = moveBindings[key][2]
                 th = moveBindings[key][3]
             elif key in speedBindings.keys():
-                speed = speed * speedBindings[key][0]
-                turn = turn * speedBindings[key][1]
+                speed_step = speed_step + speedBindings[key][0]
+                turn_step = turn_step + speedBindings[key][1]
 
-                print(vels(speed, turn))
+                print(vels(speed_step, turn_step))
                 if (status == 14):
                     print(msg)
                 status = (status + 1) % 15
             else:
-                x = 0.0
-                y = 0.0
-                z = 0.0
-                th = 0.0
+                stop = True
                 if (key == '\x03'):
                     break
 
             if stamped:
                 twist_msg.header.stamp = node.get_clock().now().to_msg()
 
-            twist.linear.x = x * speed
-            twist.linear.y = y * speed
-            twist.linear.z = z * speed
-            twist.angular.x = 0.0
-            twist.angular.y = 0.0
-            twist.angular.z = th * turn
+            if stop:
+                twist.linear.x = 0.0
+                twist.linear.y = 0.0
+                twist.linear.z = 0.0
+                twist.angular.x = 0.0
+                twist.angular.y = 0.0
+                twist.angular.z = 0.0
+            else:
+                twist.linear.x += x * speed_step
+                twist.linear.y += y * speed_step
+                twist.linear.z += z * speed_step
+                twist.angular.x += 0.0
+                twist.angular.y += 0.0
+                twist.angular.z += th * turn_step
             pub.publish(twist_msg)
 
     except Exception as e:
